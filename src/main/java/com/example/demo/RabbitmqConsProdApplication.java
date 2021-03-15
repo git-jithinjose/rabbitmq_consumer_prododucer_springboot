11c73f5c886273bbc838982cd5c95b8c6c625e65
git-jithinjose <jithinpulimoottil@gmail.com>
2021-03-15 11:32:08 +0530

    Inital update from lap

---
+++ src/main/java/com/example/demo/RabbitmqConsProdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqConsProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqConsProdApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/consumer/Consumer.java
@@ -0,0 +1,16 @@
+package com.example.demo.consumer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.config.MessagingConfig;
+import com.example.demo.dto.OrderStatus;
+
+@Component
+public class Consumer {
+
+    //@RabbitListener(queues = MessagingConfig.QUEUE)
+    public void consumeMessageFromQueue(OrderStatus orderStatus) {
+        System.out.println("Message recieved from queue : " + orderStatus);
+    }
+}
